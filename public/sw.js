9dbb0e1f41c72dc65565d3b283edaff9e4512366
Freddy Collazos <falex84@gmail.com>
2026-03-14 07:27:39 -0400

    Implement PWA: Add Service Worker and manifest updates for installability

---
+++ public/sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'carrito-ccs-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/icon.png',
+    '/src/main.jsx',
+    '/src/App.jsx',
+    '/src/index.css'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
